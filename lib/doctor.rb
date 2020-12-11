3ad06efd881d05834aff2d5d66ad2d8095ba2658
Thor Summerall <thor.summerall@gmail.com>
2020-12-11 15:41:53 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,6 +12,10 @@ class Doctor
     @@all
   end
 
+  def new_appointment (date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
   def appointments
     Appointment.all.select do |appointment|
       appointment.doctor == self
