5476157059d0e082e8287fe5b78c4b2e9db072d5
Thor Summerall <thor.summerall@gmail.com>
2020-12-11 15:12:54 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -15,11 +15,7 @@ class Genre
   def songs
     list_of_songs = []
     Song.all.select do |song|
-      if song.name == self
-        song.name << list_of_songs
-      else
-        nil
-      end
+      song.name == self
     end
     list_of_songs
   end
